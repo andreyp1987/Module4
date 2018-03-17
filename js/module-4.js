886472312296897831a8ea84eedfcd0bcdcbad27
andreyp1987 <sumyrieltor777@gmail.com>
2018-03-17 20:51:46 +0200

    v.2

--- js/module-4.js
+++ js/module-4.js
@@ -1,23 +1,31 @@
 // Используя логику создания массива клавиатурных строк, создать функцию addKeyboardLayout(alphabet) которая на входе получает строку-алфавит и возвращает массив-массивов клавиатурных строк.
 
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+const keybord =  alphabet.split('');
 function addKeyboardLayout (value) {
-const keybord =  value.split('') 
-function newLineKeybord (arr, x, y) {return arr.slice(arr.indexOf(x), arr.indexOf(y)+1)};
-const fullKeyboard = [newLineKeybord (keybord, 'q', ']'), newLineKeybord (keybord, 'a', "'"), newLineKeybord (keybord, 'z', '/')];
-return value = fullKeyboard};
+const fullKeyboard = [keybord.slice(0, keybord.indexOf('a')), keybord.slice(keybord.indexOf('a'), keybord.indexOf('z')), keybord.slice(keybord.indexOf('z'))]
+return fullKeyboard
+};
 console.log (addKeyboardLayout(alphabet));
 
 // Создать функцию getRandCharInRow(row) которая получает номер строки клавиатуры и возвращает случайную букву из этой строки.
 
 const KeyboardLayout = addKeyboardLayout(alphabet);
-function getRandCharInRow(row) {return row = KeyboardLayout[row][Math.floor(Math.random()*KeyboardLayout[row].length)]};
-alert (`Случайная буква из строки 0 = ${getRandCharInRow(0)}`);
-alert (`Случайная буква из строки 1 = ${getRandCharInRow(1)}`);
-alert (`Случайная буква из строки 2 = ${getRandCharInRow(2)}`);
+function getRandCharInRow(row) {
+    const result = KeyboardLayout[row]
+[Math.floor(Math.random()*KeyboardLayout[row].length)]
+    return result;
+};
+console.log  (`Случайная буква из строки 0 = ${getRandCharInRow(0)}`);
+console.log  (`Случайная буква из строки 1 = ${getRandCharInRow(1)}`);
+console.log  (`Случайная буква из строки 2 = ${getRandCharInRow(2)}`);
 
-// Создать функцию getRandCharInAlph() которая возвращает случайную букву из всего алфавита.
+// // Создать функцию getRandCharInAlph() которая возвращает случайную букву из всего алфавита.
 
-const keybordStr =  alphabet.split(''); 
-function getRandCharInAlph() {return keybordStr[Math.floor(Math.random()*KeyboardLayout.length)]};
-alert (`Случайная буква алфавита = ${getRandCharInAlph()}`);
+function getRandCharInAlph() {
+    const rand = Math.floor(Math.random()*(KeyboardLayout.length));
+    const result = KeyboardLayout [rand]
+[Math.floor(Math.random()*KeyboardLayout[rand].length)]
+    return result;
+};
+console.log  (`Случайная буква алфавита = ${getRandCharInAlph()}`);
