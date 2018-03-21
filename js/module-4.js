bb7af748edf38489a6f6b20a5ec097b74def46ef
andreyp1987 <sumyrieltor777@gmail.com>
2018-03-21 23:57:45 +0200

    v.3

--- js/module-4.js
+++ js/module-4.js
@@ -1,31 +1,36 @@
-// Используя логику создания массива клавиатурных строк, создать функцию addKeyboardLayout(alphabet) которая на входе получает строку-алфавит и возвращает массив-массивов клавиатурных строк.
-
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-const keybord =  alphabet.split('');
-function addKeyboardLayout (value) {
+function addKeyboardLayout (str) {
+const keybord =  str.split(''); 
 const fullKeyboard = [keybord.slice(0, keybord.indexOf('a')), keybord.slice(keybord.indexOf('a'), keybord.indexOf('z')), keybord.slice(keybord.indexOf('z'))]
 return fullKeyboard
 };
 console.log (addKeyboardLayout(alphabet));
-
-// Создать функцию getRandCharInRow(row) которая получает номер строки клавиатуры и возвращает случайную букву из этой строки.
-
 const KeyboardLayout = addKeyboardLayout(alphabet);
-function getRandCharInRow(row) {
-    const result = KeyboardLayout[row]
-[Math.floor(Math.random()*KeyboardLayout[row].length)]
-    return result;
-};
-console.log  (`Случайная буква из строки 0 = ${getRandCharInRow(0)}`);
-console.log  (`Случайная буква из строки 1 = ${getRandCharInRow(1)}`);
-console.log  (`Случайная буква из строки 2 = ${getRandCharInRow(2)}`);
 
-// // Создать функцию getRandCharInAlph() которая возвращает случайную букву из всего алфавита.
 
-function getRandCharInAlph() {
-    const rand = Math.floor(Math.random()*(KeyboardLayout.length));
-    const result = KeyboardLayout [rand]
-[Math.floor(Math.random()*KeyboardLayout[rand].length)]
-    return result;
-};
-console.log  (`Случайная буква алфавита = ${getRandCharInAlph()}`);
+function getRandomItem(arr){
+const randomItem = Math.floor(Math.random() * arr.length);
+return randomItem;
+}
+  
+function getRandCharInRow(row, callback) {
+const randomStr = KeyboardLayout[row - 1];
+const randomIndex = callback(randomStr);
+return randomStr[randomIndex];
+}
+  
+const randomCharInRow1 = getRandCharInRow(1, getRandomItem)
+const randomCharInRow2 = getRandCharInRow(2, getRandomItem)
+const randomCharInRow3 = getRandCharInRow(3, getRandomItem)
+console.log (`Случайный символ из 1-ой строки - ${randomCharInRow1}`)
+console.log (`Случайный символ из 2-ой строки - ${randomCharInRow2}`)
+console.log (`Случайный символ из 3-ой строки - ${randomCharInRow3}`)
+ 
+function getRandCharInAlph (arr, callback) {  
+const randomStr = callback(arr);
+const randomChar = callback(arr[randomStr]);  
+return arr[randomStr][randomChar];
+}
+  
+const randomAlphabetChar = getRandCharInAlph(KeyboardLayout, getRandomItem);
+console.log (`Случайный символ из случайной строки - ${randomAlphabetChar}`)
